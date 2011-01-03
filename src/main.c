1008b0c59b2122d80abca3775f0caece9674f103
David Woodhouse <dwmw2@infradead.org>
2011-01-03 02:09:15 +0000

    core: Always require thread support
    We use threads unconditionally. Even if we don't have a JS plugin.

--- src/main.c
+++ src/main.c
@@ -107,10 +107,8 @@ int main(int argc, char *argv[])
 	capng_apply(CAPNG_SELECT_BOTH);
 #endif
 
-#ifdef NEED_THREADS
 	if (g_thread_supported() == FALSE)
 		g_thread_init(NULL);
-#endif
 
 	context = g_option_context_new(NULL);
 	g_option_context_add_main_entries(context, options, NULL);
@@ -140,12 +138,10 @@ int main(int argc, char *argv[])
 
 	main_loop = g_main_loop_new(NULL, FALSE);
 
-#ifdef NEED_THREADS
 	if (dbus_threads_init_default() == FALSE) {
 		fprintf(stderr, "Can't init usage of threads\n");
 		exit(1);
 	}
-#endif
 
 	dbus_error_init(&err);
 
